0d7ca6e2e9b9eca9233e5a778c428b96a3d1fb10
Fatima Awdeh <fatimaawdeh21@gmai.com>
2023-07-26 13:23:39 +0300

    Done with TASK-5

--- paireOuImpaire.js
+++ paireOuImpaire.js
@@ -1,6 +1,6 @@
 let numbers = [1,54,7,6,878,7,86,98,21,3,9];
 for (let i=0; i < numbers.length ;i++){
-    if(i%2==0){
+    if(numbers[i] % 2 == 0){
         console.log(numbers[i] + ":" + "This number is even");
     }
     else{
